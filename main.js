0c42b535a3eeb6ab43641c36bb0547c18f7e593f
Mikele91 <michele.amoruso91.8@gmail.com>
2021-09-07 16:26:16 +0200

    aggiunti controlli

--- main.js
+++ main.js
@@ -38,14 +38,28 @@ var listaStudenti=[
 
 
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+var nome =prompt("inserisci il nome");
+while(!isNaN(nome)){
+    nome =prompt("inserisci un  nome valido");
+}
+
+var cognome = prompt("inserisci il tuo  cognome");
+while(!isNaN(cognome)){
+    nome =prompt("inserisci un  cognome valido");
+}
+
+var eta = parseInt(prompt("inserisci la tua età"));
+while(isNaN(eta)){
+    eta =parseInt(prompt("inserisci la tua età corretta"));
+}
 
 var nuovoIscritto = {
-    "nome": prompt("inserisci il nome"),
-    "cognome": prompt("inserisci il cognonome"),
-    "eta": prompt("inserisci la tua età"),
+    "nome": nome,
+    "cognome": cognome,
+    "eta": eta
 }
 listaStudenti.push(nuovoIscritto);
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 for( var i = 0; i<listaStudenti.length; i++){
-    console.log(listaStudenti[i]["nome"],listaStudenti[i]["cognome"]);
+    console.log(listaStudenti[i]["nome"],listaStudenti[i]["cognome"],listaStudenti[i]["eta"]);
 }
